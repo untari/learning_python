7ce4b719b0d362adf530f9937ee4748b59e0c8e9
tari <sastraembun0306@gmail.com>
2020-04-07 17:52:32 +0800

    if_else excercises

---
+++ if_else_statement.js
@@ -0,0 +1,43 @@
+var a = 1;
+var b = 2;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("a is less than or equal to b");
+}
+    print (a is greater or equal b)
+
+    #even and odd
+var number = 14;{
+if (number% 2 === 0 )
+console.log("even")
+}
+else{
+console.log("odd")
+}
+#output (even)
+
+#musical group excercise
+var musicians = 10;
+if (musicians === 1) {
+    console.log("solo");
+}
+else if (musicians === 2){
+     console.log("duet");
+}
+else if (musicians === 3){
+    console.log("trio");
+}
+else if (musicians === 4){
+    console.log("quater");
+}
+else if (musicians === 10){
+    console.log("this is a large group");
+}
+else{
+    console.log("isn't a group");
+}
+#output (this is large group)
+
+
